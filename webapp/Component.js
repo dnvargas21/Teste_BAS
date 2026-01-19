dce295587edeef7f47ce9ddc1bce630f56e60c70
Daniel Vargas <dn.vargas.pc@gmail.com>
2026-01-19 20:46:22 +0000

    Teste

---
+++ webapp/Component.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("project1.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
+
+//teste
